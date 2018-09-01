c5d6ccdfe8f0d56aa3873beae18e9d4220c8326b
Nguyen Thanh Tam <ntt.hcm.vn@gmail.com>
2018-09-02 00:27:42 +0700

    update code for the rest part

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,11 +4,6 @@ $(document).ready(function(){
 		//nav: true,
 		dots: true,
 		navText: ['<i class="fa fa-chevron-left"></i>', '<i class="fa fa-chevron-right"></i>'],
-		// autoHeight:true,
-		// autoWidth:true,
-		// autoplay:true,
-		// autoplayTimeout:500,
-		//center:true,
 		loop:true,
 		responsive: {
 			// breakpoint from 480 up
@@ -28,5 +23,60 @@ $(document).ready(function(){
 			}
 		}
 	});
+	$(".home-slider .owl-carousel").owlCarousel({
+		items: 1,
+		nav: true,
+		dots: false,
+		navText: ['<i class="fa fa-chevron-left"></i>', '<i class="fa fa-chevron-right"></i>'],
+		loop:true,
+		responsive: {
+			// breakpoint from 480 up
+			480: {
+				items: 1,
+			},
+			640: {
+				items: 3,
+			},
+			// breakpoint from 768 up
+			768: {
+				items: 4,
+			},
+			// breakpoint from 992 up
+			992: {
+				items: 6,
+			}
+		}
+	});
+$('.thanhtam-banner a').each(function(){
+	var e = $(this).find('.btn-detail')
+	//$(this).find('.body').hide()
+	
 
+		$(e).on('mouseover', function(){
+			$(this).addClass("animated lightSpeedOut")
+			//$(this).removeClass("animated lightSpeedOut")
+			//$(this).parents('.item').find('.body').show()
+		}).on('mouseout', function(){
+			$(this).removeClass("animated lightSpeedOut")
+			$(this).addClass("animated lightSpeedIn")
+			//$(this).parents('.item').find('.body').show()
+		})
+		
+	})
+$('.thanhtam-quote figure').each(function(){
+	var f = $(this).find('img')
+	//$(this).find('.body').hide()
+	
+
+		$(f).on('mouseover', function(){
+			$(this).addClass("animated infinite pulse")
+			//$(this).removeClass("animated lightSpeedOut")
+			//$(this).parents('.item').find('.body').show()
+		}).on('mouseout', function(){
+			$(this).removeClass("animated")
+			//$(this).parents('.item').find('.body').show()
+		})
+		
+	})
   }); 
+  
